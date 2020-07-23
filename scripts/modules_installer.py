ff3af96554e19825b4fc6fb7bf4c89caf00e265a
enzo <enzo.ccc@gmail.com>
2020-07-23 11:12:22 +0200

    Add files via upload

--- scripts/modules_installer.py
+++ scripts/modules_installer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 /***************************************************************************
-    pyArchInit Plugin  - A QGIS plugin to manage archaeological dataset
+    HFF_system Plugin  - A QGIS plugin to manage archaeological dataset
     stored in Postgres
     -------------------
     begin                : 2018-04-22
@@ -18,38 +18,44 @@
  ***************************************************************************/
 """
 
-
-
 import subprocess
 import sys
-
+import platform
+#from .. modules.utility.pyarchinit_OS_utility import Pyarchinit_OS_Utility
 
 packages = sys.argv[1].split(',') if len(sys.argv) >= 2 else []
 
 # Adding the dependencies python modules in
 # package list in order to install via pip module
 
-    
+
 if not packages:
-    packages = [ 
-                'SQLAlchemy',
-                'SQLAlchemy-Utils',
-                'reportlab',
-                'networkx',
-                'matplotlib',
-                'pandas',
-                'psycopg2',
-                'elasticsearch',
-                'pysftp',
-                'xlsxwriter'
-                ]
+    packages = [
+        'SQLAlchemy',
+        'SQLAlchemy-Utils',
+        'reportlab',
+        'networkx',
+        'matplotlib',
+        'pandas',
+        'psycopg2',
+        'elasticsearch',
+        'pysftp',
+        'xlsxwriter'
+    ]
+python_path = sys.exec_prefix
+python_version = sys.version[:3]
 
+if platform.system()=='Windows':
+    cmd = '{}\python'.format(python_path)
+elif platform.system()=='Darwin':
+    cmd = '{}/bin/python{}'.format(python_path, python_version)
+else:
+    cmd = '{}/bin/python{}'.format(python_path, python_version)
 
+# install pip if it is not found
+subprocess.check_call([cmd, '-m', 'ensurepip'], shell=False)
 
-
-
-subprocess.check_call([sys.executable, '-m', 'ensurepip'], shell=False)
-    
 for p in packages:
+    subprocess.check_call([cmd, '-m', 'pip', 'install', '--upgrade', p,'--user'], shell=False)
 
-    subprocess.check_call([sys.executable, '-m', 'pip' ,'install','--upgrade', p], shell=False)
+
