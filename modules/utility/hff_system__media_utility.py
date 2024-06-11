350c6731365437d9d9bc665e1e007be1d9964a0a
enzo <enzo.ccc@gmail.com>
2024-06-11 12:25:53 +0200

    Update thumbnail resizing method to use LANCZOS for improved image quality.

--- modules/utility/hff_system__media_utility.py
+++ modules/utility/hff_system__media_utility.py
@@ -37,7 +37,7 @@ class Media_utility(object):
 >>> width, height = (150, 110)        # ridimensiono l'immagine l' originale era 300x350px
 >>> size = (width, height)            # assegno grandezza e altezza
 >>> new_image_thumbnail = r"C:\test\snake_small.png" # che sia pitone grande o  pitone piccolo l' importante che pitone è
->>> image.thumbnail(size, Image.ANTIALIAS) # creo la thumbnail vera e propria con antialias ma potevamo anche bicubic bilinear ecc
+>>> image.thumbnail(size, Image.LANCZOS) # creo la thumbnail vera e propria con antialias ma potevamo anche bicubic bilinear ecc
 >>> image.save(new_image_thumbnail)        # salvo la nuova immagine "thumbnail"
 """
 
@@ -53,7 +53,7 @@ class Media_utility(object):
         outfile = ('%s%s_%s%s') % (
         self.outpath, str(self.max_num_id), os.path.splitext(self.infile)[0], self.thumb_suffix)
         im = Image.open(infile)
-        im.thumbnail(size, Image.ANTIALIAS)
+        im.thumbnail(size, Image.LANCZOS)
         im.save(outfile, dpi=(100, 100))
 
 
@@ -73,7 +73,7 @@ class Media_utility_resize(object):
 >>> width, height = (150, 110)        # ridimensiono l'immagine l' originale era 300x350px
 >>> size = (width, height)            # assegno grandezza e altezza
 >>> new_image_thumbnail = r"C:\test\snake_small.png" # che sia pitone grande o  pitone piccolo l' importante che pitone è
->>> image.thumbnail(size, Image.ANTIALIAS) # creo la thumbnail vera e propria con antialias ma potevamo anche bicubic bilinear ecc
+>>> image.thumbnail(size, Image.LANCZOS) # creo la thumbnail vera e propria con antialias ma potevamo anche bicubic bilinear ecc
 >>> image.save(new_image_thumbnail)        # salvo la nuova immagine "thumbnail"
 """
 
@@ -89,7 +89,7 @@ class Media_utility_resize(object):
         outfile = ('%s%s_%s%s') % (
         self.outpath, str(self.max_num_id), os.path.splitext(self.infile)[0], self.thumb_suffix)
         im = Image.open(infile)
-        im.thumbnail(size, Image.ANTIALIAS)
+        im.thumbnail(size, Image.LANCZOS)
         im.save(outfile, dpi=(300, 300))
 
 
